90cb7ef4e6f2a41f9b95b1b2fcab23a95f87ac04
mohammad ashour <moh.ash96@gmail.com>
2021-03-29 13:19:35 +0300

    should be done

--- server.js
+++ server.js
@@ -1,103 +1,71 @@
-'use strict';
+"use strict";
 
-const dotenv = require('dotenv'); //dotenv allows us to load environment variables from a .env file
+const dotenv = require("dotenv"); //dotenv allows us to load environment variables from a .env file
 dotenv.config();
 const PORT = process.env.PORT || 3000; // get the port from the environment
 
-const express = require('express'); // load express module, used to create a web server
+const express = require("express"); // load express module, used to create a web server
 const app = express(); // create an express application which we'll use as our web server
-const { response } = require('express');
+const { response } = require("express");
 
-
-const cors = require('cors'); // load the cors library, it allows our server to accept APIs calls from other domains
-app.use(cors()); 
+const cors = require("cors"); // load the cors library, it allows our server to accept APIs calls from other domains
+app.use(cors());
 // Request -> cors -> handle -> *... -> responses
 
-app.get('/location', handleLocation); // handle GET calls to the /location path using handleLocation route handler
+app.get("/location", handleLocation); // handle GET calls to the /location path using handleLocation route handler
 
-app.get('/weather', handleWeather); // handle GET calls to the /weather path using handleWeather route handler
+app.get("/weather", handleWeather); // handle GET calls to the /weather path using handleWeather route handler
 
-app.get('*', handleErrors); // handle any other route using handleErrors route handler
+app.get("*", handleErrors); // handle any other route using handleErrors route handler
 
-app.listen(PORT, () =>{
-    console.log(`server is listening to port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`server is listening to port ${PORT}`);
 });
 
-function Locations(search, format, lat, lon){
-    this.search_query = search;
-    this.formatted_query = format;
-    this.latitude = lat;
-    this.longitude = lon;
+function Locations(search, format, lat, lon) {
+  this.search_query = search;
+  this.formatted_query = format;
+  this.latitude = lat;
+  this.longitude = lon;
 }
 
+function handleLocation(req, res) {
+  let search = req.query.city; // assign the value found in the city query parameter to search
 
-
-
-function handleLocation (req, res){
-    let search = req.query.city // assign the value found in the city query parameter to search
-    
-    try {
-        const location = require('./data/location.json'); // creates location variable and loads the content of the location.json
-        let obj = location[0]; // gets the first object in the obj array 
-        let newLocation = new Locations(search, obj.display_name, obj.lat, obj.lon); // declare a variable called newLocation and assign to it new Location instatnce
-        // let foundLocation = null;
-        // location.forEach(place =>{
-        //     if (location.display_name === req.query.name){
-        //         foundLocation = place;
-        //     }
-        // });
-        res.status(200).send(newLocation);
-    }
-    
-    catch{
-        res.status(404).send('Something Went Wrong');
-    }
+  try {
+    const location = require("./data/location.json"); // creates location variable and loads the content of the location.json
+    let obj = location[0]; // gets the first object in the obj array
+    let newLocation = new Locations(search, obj.display_name, obj.lat, obj.lon); // declare a variable called newLocation and assign to it new Location instatnce
+    res.status(200).send(newLocation);
+  } catch {
+    res.status(404).send("Something Went Wrong");
+  }
 }
 
-
-
-function Weather (search, forecast, time){
-    this.search_query = search;
-    this.forecast = forecast;
-    this.time = time;
-    // newArr.push(this);
-    
+function Weather(search, forecast, time) {
+  this.search_query = search;
+  this.forecast = forecast;
+  this.time = time;
 }
 
+function handleWeather(req, res) {
+  let search = req.query.city;
+  let weatherObj = require("./data/weather.json");
+  let newArr = [];
+  let dataArray = weatherObj.data;
+  dataArray.forEach((section) => {
+    let description = section.weather["description"];
+    let dates = section.datetime;
+    let newWeather = new Weather(search, description, dates);
+    newArr.push(newWeather);
+  });
 
-function handleWeather (req, res){
-    let search = req.query.city;
-    let weatherObj = require('./data/weather.json');
-    let newArr = [];
-    let dataArray = weatherObj.data;
-    dataArray.forEach( section =>{
-        let description = section.weather['description'];
-        let dates = section.datetime
-        let newWeather = new Weather(search, description, dates);
-        newArr.push(newWeather)
-    });
-    
-    // let foundWeather = null;
-    // dataArray.forEach(place =>{
-    //     if (dataArray.city_name === req.query.name){
-    //         foundWeather = place;
-    //     }
-    // });
-    // if(foundWeather){
-        try{
-            // newArr;
-
-            res.status(200).send(newArr);
-        }
-    // } else{
-        catch{
-
-            res.status(500).send('Something Went Wrong');
-        }
-    // }
+  try {
+    res.status(200).send(newArr);
+  } catch {
+    res.status(500).send("Something Went Wrong");
+  }
 }
-
-function handleErrors(){
-    res.status(500).send('Error')
+function handleErrors() {
+  res.status(500).send(`Something Went Wrong ${error}`);
 }
-
