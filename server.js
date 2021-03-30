0f4e2ba1a38fe8c63447fdc14eb6c433b14c3612
mohammad ashour <moh.ash96@gmail.com>
2021-03-30 12:51:22 +0300

    finished weather

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 "use strict";
-
-const superagent = require('superagent');
+// Load environment variables
 const dotenv = require("dotenv"); //dotenv allows us to load environment variables from a .env file
 dotenv.config();
-const PORT = process.env.PORT || 3001; // get the port from the environment
 
 const express = require("express"); // load express module, used to create a web server
-const app = express(); // create an express application which we'll use as our web server
-const { response } = require("express");
-
+const superagent = require('superagent');
 const cors = require("cors"); // load the cors library, it allows our server to accept APIs calls from other domains
+
+// const { response } = require("express");
+
+// setup application
+const app = express(); // create an express application which we'll use as our web server
+
+const PORT = process.env.PORT || 3001; // get the port from the environment
+
+
 app.use(cors());
-// Request -> cors -> handle -> *... -> responses
+
 
 app.get("/location", handleLocation); // handle GET calls to the /location path using handleLocation route handler
-
 app.get("/weather", handleWeather); // handle GET calls to the /weather path using handleWeather route handler
-
-app.get("*", handleErrors); // handle any other route using handleErrors route handler
+app.use("*", handleErrors); // handle any other route using handleErrors route handler
 
 
 app.listen(PORT, () => {
   console.log(`server is listening to port ${PORT}`);
 });
+
+
+
+
 
 function Locations(city, geoData) {
   this.search_query = city;
@@ -37,63 +44,63 @@ function handleLocation(req, res) {
   let city = req.query.city; // assign the value found in the city query parameter to search
   let key = process.env.GEOCODE_API_KEY;
   const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
- 
-  if (locations[url]){
+
+  if (locations[url]) {
     res.send(locations[url]);
-  }else{
+    // console.log(locations[url]);
+  } else {
     superagent.get(url)
-    .then(data =>{
-      // const location = require("./data/location.json"); // creates location variable and loads the content of the location.json
-      
-      const geoData = data.body[0]; // gets the first object in the obj array
-      let location = new Locations(city, geoData); // declare a variable called newLocation and assign to it new Location instatnce
-      locations[url] = location;
-      res.status(200).send(location);
-    }) 
-    .catch (() =>{
-      res.status(404).send("Something Went Wrong");
+      .then(data => {
+        // const location = require("./data/location.json"); // creates location variable and loads the content of the location.json
+        const geoData = data.body[0]; // gets the first object in the obj array
+        let location = new Locations(city, geoData); // declare a variable called newLocation and assign to it new Location instatnce
+        locations[url] = location;
+        res.status(200).send(location);
+        // console.log(location);
+      })
+      .catch(() => {
+        res.status(404).send("Something Went Wrong");
 
-    })
-    }
+      })
   }
-
-
-function Weather(city, forecast, time) {
-  this.search_query = city;
-  this.forecast = forecast;
-  this.time = time;
 }
 
-function handleWeather(req, res) {
-  let city = req.query.city;
-  let weatherObj = require("./data/weather.json");
-  let newArr = [];
-  let dataArray = weatherObj.data;
-  // dataArray.forEach((section) => {
-  //   let description = section.weather["description"];
-  //   let dates = section.datetime;
-  //   let newWeather = new Weather(search, description, dates);
-  //   newArr.push(newWeather);
-  // });
-  let reformattedArray = dataArray.map(obj => {
-    let rObj = {}
-    rObj[obj.key] = obj.value
-      let description = obj.weather["description"];
-      let dates = obj.datetime;
-      let newWeather = new Weather(city, description, dates);
-      newArr.push(newWeather);
-})
 
+function Weather(forecast, valid_date) {
+  this.forecast = forecast;
+  this.valid_date = valid_date;
+}
+const weatherInfo = [];
+
+async function handleWeather(req, res) {
   try {
-    res.status(200).send(newArr);
-  } catch {
+    let key = process.env.WEATHER_API_KEY;
+    let lat = req.query.latitude;
+    let lon = req.query.longitude;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`
+    const rawWeatherData = await superagent.get(url);
+
+    let dataArray = JSON.parse(rawWeatherData.text).data;
+
+    let reformattedArray = dataArray.map(obj => {
+      let description = obj.weather.description;
+      let dates = obj.valid_date;
+      return new Weather(description, dates);
+    })
+    res.json(reformattedArray);
+  }
+  catch (error) {
+
     res.status(500).send("Something Went Wrong");
   }
 }
-function handleErrors(req,res) {
-  res.status(500).send({
+
+
+
+function handleErrors(req, res) {
+  res.status(404).send({
     status: 500,
     responseText: "Sorry, something went wrong",
-    
+
   });
 };
